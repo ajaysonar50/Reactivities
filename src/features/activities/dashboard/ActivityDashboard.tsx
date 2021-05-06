1fdb6320f11f96dea34b72e6b537cc3a5cf83682
Ajay Sonar <Ajay.Sonar@ey.com>
2021-05-07 01:29:26 +0530

    CreateViewEditClieckWorks

--- src/features/activities/dashboard/ActivityDashboard.tsx
+++ src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,21 +1,35 @@
 import { Grid } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
 import ActivityList from "./ActivityList";
 
 interface Props {
-    activities: Activity[]
+    activities: Activity[];
+    selectedActivity: Activity | undefined;
+    selectActivity: (id: string) => void;
+    cancelSelectActivity: () => void;
+    editMode: boolean;
+    openForm: (id: string) => void;
+    closeForm: () => void;
 }
 
-export default function ActivityDashboard({activities}: Props) {
+export default function ActivityDashboard({activities, selectActivity, cancelSelectActivity, selectedActivity, editMode, openForm, closeForm}: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList activities={activities} />
+                <ActivityList activities={activities} selectActivity={selectActivity} />
             </Grid.Column>
             <Grid.Column width='6'>
                 {
-                    activities[0] && <ActivityDetails activity={activities[0]} />
+                    selectedActivity && !editMode &&
+                    <ActivityDetails activity={selectedActivity}
+                     cancelSelectActivity={cancelSelectActivity} 
+                     openForm={openForm}
+                     />
+                }
+                {
+                    editMode && <ActivityForm closeForm={closeForm} activity={selectedActivity}/>
                 }
             </Grid.Column>
         </Grid>
